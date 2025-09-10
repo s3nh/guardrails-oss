79f10a860f639ed741076594494a1b7a77383497
s3nh <17111860+s3nh@users.noreply.github.com>
2025-09-10 21:22:47 +0200

    Initialize __init__.py for anonymize module

---
+++ anonymize/__init__.py
@@ -0,0 +1,12 @@
+from .anonymize import (
+    AnonymizeConfig,
+    AnonymizationResult,
+    anonymize_text,
+    load_name_dictionaries,
+)
+__all__ = [
+    "AnonymizeConfig",
+    "AnonymizationResult",
+    "anonymize_text",
+    "load_name_dictionaries",
+]
